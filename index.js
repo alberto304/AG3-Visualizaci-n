1e152f18974bb567b8b05acd1eb5ba7577aa725d
alberto304 <71696670+alberto304@users.noreply.github.com>
2023-05-03 00:24:40 +0200

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Visualización AG3</h1>`;
+
+var visualization = d3plus.viz()
+  .container("#viz1")
+  .data([{"year":1991, "name":"alpha", "value":15},{"year":1992, "name":"alpha", "value":20}])
+  .type("bar")
+  .id("name")
+  .x("year")
+  .y("value")
+  .draw()
+  
