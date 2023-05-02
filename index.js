ae1db4010640f9395044b4a225d149ad1737f96b
alberto304 <71696670+alberto304@users.noreply.github.com>
2023-05-03 00:27:33 +0200

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Visualización AG3</h1>`;
 
+d3.csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv", function(error, data)
+{
+if(error){console.log("Error al leer el fichero titanic.csv");
+} else {
+//Incorporación de gráficos
+// .....
+}
+});
+
 var visualization = d3plus
   .viz()
   .container('#viz1')
